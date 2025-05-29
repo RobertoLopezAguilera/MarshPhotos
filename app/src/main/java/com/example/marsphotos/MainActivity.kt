18d9101e8019f8128e999882f965203e8627efa7
RobertoLopezAguilera <98922244+RobertoLopezAguilera@users.noreply.github.com>
2025-05-29 14:19:01 -0600

    Update MainActivity.kt
    dagger

--- app/src/main/java/com/example/marsphotos/MainActivity.kt
+++ app/src/main/java/com/example/marsphotos/MainActivity.kt
@@ -16,6 +16,8 @@
 
 package com.example.marsphotos
 
+import android.app.Activity //
+import android.app.Application //
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,11 +25,68 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.marsphotos.data.LoginComponent
+import com.example.marsphotos.data.LoginComponentProvider //
 import com.example.marsphotos.ui.MarsPhotosApp
 import com.example.marsphotos.ui.theme.MarsPhotosTheme
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+
+
+class LoginActivity: Activity() {
+
+    // You want Dagger to provide an instance of LoginViewModel from the Login graph
+    @Inject lateinit var loginViewModel: LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Gets appComponent from MyApplication available in the base Gradle module
+        val appComponent = (applicationContext as MyApplication).appComponent
+
+        // Creates a new instance of LoginComponent
+        // Injects the component to populate the @Inject fields
+       // DaggerLoginComponent.factory().create(appComponent).inject(this)
+
+        super.onCreate(savedInstanceState)
+
+        // Now you can access loginViewModel
+    }
+}
+
+// LoginViewModel depends on UserRepository that is scoped to AppComponent
+class LoginViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) {  }
+// UserRepository's dependencies
+class UserLocalDataSource @Inject constructor() {  }
+class UserRemoteDataSource @Inject constructor() {  }
+
+// UserRepository is scoped to AppComponent
+@Singleton
+class UserRepository @Inject constructor(
+    private val localDataSource: UserLocalDataSource,
+    private val remoteDataSource: UserRemoteDataSource
+) {  }
+
+class MyApplication: Application(), LoginComponentProvider {
+    // Reference to the application graph that is used across the whole app
+    val appComponent = DaggerApplicationComponent.create()
+   //Causa error el create
+
+    override fun provideLoginComponent(): LoginComponent {
+        return appComponent.loginComponent().create()
+    }
+}
+
+class DaggerApplicationComponent {
+
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        var loginComponent = (applicationContext as LoginComponentProvider)
+            .provideLoginComponent()
+
+        loginComponent.inject(this)
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
